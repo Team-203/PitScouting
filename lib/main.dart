2075a2a944c87e1b8437653faebf97f81ee0b533
pentrail <adamw@tutamail.com>
2023-04-01 16:26:32 -0400

    Release 1

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Team 203 Pit Scouting App',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -22,9 +22,19 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
+        fontFamily: 'SofiaSans',
+        scaffoldBackgroundColor: Colors.black12,
+        brightness: Brightness.dark,
+        appBarTheme: AppBarTheme(
+          color: Colors.red,
+          iconTheme: IconThemeData(
+            color: Colors.white,
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Team 203 Pit Scouting App'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -47,18 +57,26 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class SecondPage extends StatefulWidget {
+  const SecondPage({Key? key, required this.title}) : super(key: key);
+  final String title;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  State<SecondPage> createState() => _SecondPageState();
+}
+
+final scouterName = TextEditingController();
+final teamNumber = TextEditingController();
+
+final _homePageFormKey = GlobalKey<FormState>();
+final _secondPageFormKey = GlobalKey<FormState>();
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    teamNumber.dispose();
+    super.dispose();
   }
 
   @override
@@ -75,41 +93,119 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: Form(
+        key: _homePageFormKey,
+        child: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('assets/frc203.png'),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  keyboardType: TextInputType.name,
+                  controller: scouterName,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter your full name';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter Your Full Name',
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextFormField(
+                  keyboardType: TextInputType.number,
+                  controller: teamNumber,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter the team number you are scouting';
+                    }
+                    return null;
+                  },
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter Team Number You Are Scouting',
+                  ),
+                ),
+              ),
+              Container(
+                  margin: const EdgeInsets.all(50),
+                  child: ButtonTheme(
+                      minWidth: 500,
+                      height: 100,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            shape: MaterialStateProperty.all<
+                                    RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(12.0),
+                                    side: const BorderSide(color: Colors.red))),
+                            padding: MaterialStateProperty.all<EdgeInsets>(
+                                const EdgeInsets.fromLTRB(50, 25, 50, 25))),
+                        onPressed: () {
+                          if (_homePageFormKey.currentState!.validate()) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const SecondPage(
+                                        title: 'Scouting Data',
+                                      )),
+                            );
+                          }
+                        },
+                        child: const Text('Continue',
+                            style: TextStyle(fontSize: 25)),
+                      ))),
+              const Text('Please Return To Team 203 Pit If Found',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class _SecondPageState extends State<SecondPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text("Scouting Data - Team " + teamNumber.text),
+      ),
+      body: Form(
+        key: _secondPageFormKey,
+        child: ListView(
+            // Center is a layout widget. It takes a single child and positions it
+            // in the middle of the parent.
+            children: <Widget>[]),
+      ),
     );
   }
 }
